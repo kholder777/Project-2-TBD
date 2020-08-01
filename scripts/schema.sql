4170b626ae5709ad49b19cab995d04c5d75a7ca1
Jordan <jordan.a.neill@gmail.com>
2020-08-01 16:53:45 -0500

    added categories to put into the database so the user has a category to choose from when adding a new item to their currents list

--- scripts/schema.sql
+++ scripts/schema.sql
@@ -3,28 +3,99 @@ CREATE DATABASE bagit_DB;
 
 USE bagit_DB;
 
-CREATE TABLE category(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+CREATE TABLE category
+(
+  id INT NOT NULL
+  AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR
+  (30) NOT NULL
   );
 
-CREATE TABLE user(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+  CREATE TABLE user
+  (
+    id INT NOT NULL
+    AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR
+    (30) NOT NULL
   );
 
 
-CREATE TABLE items(
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+    CREATE TABLE items
+    (
+      id INT NOT NULL
+      AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR
+      (30) NOT NULL,
   active BOOLEAN,
   fav BOOLEAN,
   catID INT NOT NULL,
-     FOREIGN KEY (catID)
-     REFERENCES category(id)
-     ON DELETE CASCADE,
-  userId INT NOT NULL,
-    FOREIGN KEY (userId)
-    REFERENCES user(id)
-    ON DELETE CASCADE
+     FOREIGN KEY
+      (catID)
+     REFERENCES category
+      (id)
+     ON
+      DELETE CASCADE,
+  userId INT
+      NOT NULL,
+    FOREIGN KEY
+      (userId)
+    REFERENCES user
+      (id)
+    ON
+      DELETE CASCADE
     );
+
+      -- Set the DB
+      USE bagit_DB;
+      -- Populate users
+      INSERT INTO user
+        (id,name)
+      VALUES
+        (1, 'Sam'),
+        (2, 'Harry P'),
+        (3, 'Tyrion'),
+        (4, 'Voldamort'),
+        (5, 'Melkor');
+
+      -- Populate Roles 
+      INSERT INTO category
+        (id,name)
+      VALUES
+        (1, 'Bread'),
+        (2, 'Meat'),
+        (3, 'Dairy'),
+        (4, 'Produce'),
+        (5, 'Seafood'),
+        (6, 'Canned Goods'),
+        (7, 'Baking and Cooking'),
+        (8, 'Snacks'),
+        (9, 'Drinks'),
+        (10, 'Wine and Beer'),
+        (11, 'Hygiene'),
+        (12, 'Household Items'),
+        (12, 'Bathroom'),
+        (13, 'Laundry'),
+        (14, 'Baby Stuff'),
+        (15, 'Cosmetics'),
+        (16, 'Paper Products'),
+        (17, 'Medicine/Remedies'),
+        (18, 'Freezer'),
+        (19, 'Oils'),
+        (20, 'Spices'),
+        (21, 'Pasta'),
+        (22, 'Cleaning Products'),
+        (23, 'Deli'),
+        (24, 'General'),
+        (25, 'Uncategorized')
+      ;
+
+      -- Populate Employees
+
+      INSERT INTO items
+        (id,name,active,fav,catID,userID)
+      VALUES
+        (001, 'Toothpaste', true, true, 1, 1),
+        (002, 'Butter Beer', true, true, 10, 2),
+        (003, 'Brisket', true, false, 2, 3),
+        (004, 'Beef Jerky', false, false, 8, 4),
+        (005, 'Yeast', false, true, 7, 4);
