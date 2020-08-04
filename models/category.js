897668d818a3285f0cc7061ea23f58181edc6e85
juanlajara <juanlajara001@gmail.com>
2020-08-03 23:09:36 -0500

    + free tables

--- models/category.js
+++ models/category.js
@@ -1,24 +1,28 @@
 //creating category table.
 
 module.exports = function (sequelize, DataTypes) {
-  var Category = sequelize.define("category", {
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    catID: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-  });
+	var Category = sequelize.define(
+		"Category",
+		{
+			category_name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			catID: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+		},
+		{ freezeTableName: true }
+	);
 
-  Category.associate = function (models) {
-    // Associating Category with items
-    Category.hasMany(models.items, {
-      onDelete: "cascade",
-    });
-  };
+	Category.associate = function (models) {
+		// Associating Category with items
+		Category.hasMany(models.items, {
+			onDelete: "cascade",
+		});
+	};
 
-  return Category;
+	return Category;
 };
