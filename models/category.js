019f1748c3e955e967738e8907cfe7ac2fba836e
Ashley Lerma <lerma.scout@gmail.com>
2020-08-03 23:02:06 -0500

    correct capitalization

--- models/category.js
+++ models/category.js
@@ -1,24 +1,24 @@
 //creating category table.
 
 module.exports = function (sequelize, DataTypes) {
-	var category = sequelize.define("category", {
-		category_name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		catID: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true,
-		},
-	});
+  var Category = sequelize.define("category", {
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    catID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  });
 
-	category.associate = function (models) {
-		// Associating Category with items
-		category.hasMany(models.items, {
-			onDelete: "cascade",
-		});
-	};
+  Category.associate = function (models) {
+    // Associating Category with items
+    Category.hasMany(models.items, {
+      onDelete: "cascade",
+    });
+  };
 
-	return category;
+  return Category;
 };
