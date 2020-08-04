6bf90d1d6ab40478eed7294307a6b73cbcd46494
Jordan <jordan.a.neill@gmail.com>
2020-08-04 00:42:40 -0500

    figuring out styling with bootstrap

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -3,58 +3,60 @@
 
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 import "./style.css";
 
 class Form extends Component {
-	// Setting the component's initial state
-	state = {
-		userName: "",
-	};
+  // Setting the component's initial state
+  state = {
+    userName: "",
+  };
 
-	handleInputChange = (event) => {
-		// Getting the value and name of the input which triggered the change
-		const { name, value } = event.target;
+  handleInputChange = (event) => {
+    // Getting the value and name of the input which triggered the change
+    const { name, value } = event.target;
 
-		// Updating the input's state
-		this.setState({
-			[name]: value,
-		});
-	};
+    // Updating the input's state
+    this.setState({
+      [name]: value,
+    });
+  };
 
-	handleFormSubmit = (event) => {
-		// Preventing the default behavior of the form submit (which is to refresh the page)
-		event.preventDefault();
+  handleFormSubmit = (event) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
 
-		// Alert the user their username, clear `this.stateuserName` clearing the inputs
-		alert(`Hello ${this.state.userName}`);
-		this.setState({
-			userName: "",
-		});
-	};
+    // Alert the user their username, clear `this.stateuserName` clearing the inputs
+    alert(`Hello ${this.state.userName}`);
+    this.setState({
+      userName: "",
+    });
+  };
 
-	render() {
-		// greets user
-		return (
-			<div>
-				{/* {<Container>
-					<Navbar fixed="top" expand="lg" variant="dark" bg="dark">
-						<Navbar.Brand href="#">Navbar</Navbar.Brand>
-					</Navbar>
-				</Container>} */}
-				<p>Hello {this.state.userName}</p>
-				<form className="form">
-					<input
-						value={this.state.userName}
-						name="userName"
-						onChange={this.handleInputChange}
-						type="text"
-						placeholder="Username"
-					/>
-					<button onClick={this.handleFormSubmit}>login</button>
-				</form>
-			</div>
-		);
-	}
+  render() {
+    // greets user
+    return (
+      <div>
+        <Container>
+          <Navbar fixed="top" expand="lg" variant="dark" bg="dark">
+            <Navbar.Brand href="#">Navbar</Navbar.Brand>
+          </Navbar>
+        </Container>
+        <p>Hello {this.state.userName}</p>
+        <form className="form">
+          <input
+            value={this.state.userName}
+            name="userName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Username"
+          />
+          <button onClick={this.handleFormSubmit}>login</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Form;
