91a3e1b13a9a962f7adf241387a7160cdbed2a2f
Kevin Holder <kholder777@yahoo.com>
2020-08-05 12:04:58 -0500

    Divided the row into 4 columns. Star has been changed into a button. Need to do some css modifications to the button. If selected, button should change colors.

--- client/src/components/Form/homePage.js
+++ client/src/components/Form/homePage.js
@@ -6,8 +6,12 @@ function HomePage() {
 	//if LoginForm is filled out and true, proceed to homePage.js
 	return (
 		<div>
-			<SearchBar />
-			<Department />
+			<header>
+				<SearchBar />
+			</header>
+			<div>
+				<Department />
+			</div>
 		</div>
 	);
 }
