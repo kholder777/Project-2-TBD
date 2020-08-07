4e5595d06c8a230fccd9f15481a99f8471c5d12a
juanlajara <juanlajara001@gmail.com>
2020-08-06 21:20:54 -0500

    + in/active/all items api and postman assets

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -2,17 +2,56 @@ const router = require("express").Router();
 var db = require("../../models");
 
 // get route, edited to match sequelize
-router.get("/items", function (req, res) {
+router.get("/active", function (req, res) {
 	// replace old function with sequelize function
-	db.User.findAll({})
+	db.items
+		.findAll({
+			where: {
+				active: 1,
+			},
+		})
 		// use promise method to pass the userlist...
-		.then(function (dbuser) {
+		.then(function (dbitems) {
 			// into the main index, updating the page
-			console.log(dbuser);
-			var dbuser = {
-				user: dbuser,
+			console.log(dbitems);
+			var dbitems = {
+				items: dbitems,
 			};
-			return res.json(dbuser);
+			return res.json(dbitems);
+		});
+});
+
+router.get("/inactive", function (req, res) {
+	// replace old function with sequelize function
+	db.items
+		.findAll({
+			where: {
+				active: 0,
+			},
+		})
+		// use promise method to pass the userlist...
+		.then(function (dbitems) {
+			// into the main index, updating the page
+			console.log(dbitems);
+			var dbitems = {
+				items: dbitems,
+			};
+			return res.json(dbitems);
+		});
+});
+
+router.get("/all", function (req, res) {
+	// replace old function with sequelize function
+	db.items
+		.findAll({})
+		// use promise method to pass the userlist...
+		.then(function (dbitems) {
+			// into the main index, updating the page
+			console.log(dbitems);
+			var dbitems = {
+				items: dbitems,
+			};
+			return res.json(dbitems);
 		});
 });
 
