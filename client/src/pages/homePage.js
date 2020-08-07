96d67d56ceaed978b2a281a9a2ad4f3cf033c9e4
juanlajara <juanlajara001@gmail.com>
2020-08-07 14:56:16 -0500

    +  Kevin's items list & commented out NavBar

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -2,21 +2,23 @@ import React from "react";
 // import Department from "../components/department";
 import AddBar from "../components/AddBar";
 import NavBar from "../components/navBar";
+import Department from "../components/department";
+import ItemBar from "../components/itemBar";
 
 function HomePage() {
-  //if LoginForm is filled out and true, proceed to homePage.js
-  //   const array = [1, 2, 3];
+	//if LoginForm is filled out and true, proceed to homePage.js
+	const array = [1, 2, 3];
 
-  return (
-    <div>
-      <NavBar />
-      <AddBar />
-      {/* //map this department */}
-      {/* {array.map((DeptArray) => {
-        return <Department number={DeptArray} />;
-      })} */}
-      {/* Array of departments. Map departments too. Look into props.*/}
-    </div>
-  );
+	return (
+		<div>
+			{/* <NavBar /> */}
+			<AddBar />
+			{/* //map this department */}
+			{array.map((DeptArray) => {
+				return <Department number={DeptArray} />;
+			})}
+			{/* Array of departments. Map departments too. Look into props.*/}
+		</div>
+	);
 }
 export default HomePage;
