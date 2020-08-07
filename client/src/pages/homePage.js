0ef4fbf68ad37c759758e0ae800d6e9aadb3e482
Ashley Lerma <lerma.scout@gmail.com>
2020-08-07 00:33:42 -0500

    added nav bar

--- client/src/pages/homePage.js
+++ client/src/pages/homePage.js
@@ -1,20 +1,20 @@
 import React from "react";
-import Department from "../components/department";
+// import Department from "../components/department";
 import AddBar from "../components/AddBar";
-import LoginButton from "../components/loginButton";
+import NavBar from "../components/navBar";
 
 function HomePage() {
   //if LoginForm is filled out and true, proceed to homePage.js
-  const array = [1, 2, 3];
+  //   const array = [1, 2, 3];
 
   return (
     <div>
-      <LoginButton />
+      <NavBar />
       <AddBar />
       {/* //map this department */}
-      {array.map((DeptArray) => {
+      {/* {array.map((DeptArray) => {
         return <Department number={DeptArray} />;
-      })}
+      })} */}
       {/* Array of departments. Map departments too. Look into props.*/}
     </div>
   );
