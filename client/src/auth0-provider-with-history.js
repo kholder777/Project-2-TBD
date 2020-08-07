a59b0afde49f38c4cff1e7c0742f4c3347786dff
Ashley Lerma <lerma.scout@gmail.com>
2020-08-06 23:48:15 -0500

    moved env to client folder

--- client/src/auth0-provider-with-history.js
+++ client/src/auth0-provider-with-history.js
@@ -1,17 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { useHistory } from "react-router-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-ReactDOM.render(
-  <Auth0Provider
-    domain="dev-bvb5jton.us.auth0.com"
-    clientId="Ot86tbijaCqs37Ezh7db0TV1C3Om9rbr"
-    redirectUri={window.location.origin}
-  >
-    <App />
-  </Auth0Provider>,
-  document.getElementById("root")
-);
+const Auth0ProviderWithHistory = ({ children }) => {
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
-export default Auth0Provider;
+  const history = useHistory();
+
+  const onRedirectCallback = (appState) => {
+    history.push(appState?.returnTo || window.location.pathname);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithHistory;
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import Auth0Provider from "./auth0-provider-with-history";
+import Auth0ProviderWithHistory from "./auth0-provider-with-history";
 
 ReactDOM.render(
   <Router>
-    <Auth0Provider>
+    <Auth0ProviderWithHistory>
       <App />
-    </Auth0Provider>
+    </Auth0ProviderWithHistory>
   </Router>,
   document.getElementById("root")
 );
