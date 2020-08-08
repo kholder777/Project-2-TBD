c8b3847d0dc0793f5f76d68de02c5bd6e6993277
Ashley Lerma <lerma.scout@gmail.com>
2020-08-08 16:14:02 -0500

    cleaned up comments to reduce clutter

--- client/src/components/private-route.js
+++ client/src/components/private-route.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import { Loading } from "./index";
 
+// Creates a loading screen to show user that something is processing
 const PrivateRoute = ({ component, ...args }) => (
   <Route
     component={withAuthenticationRequired(component, {
