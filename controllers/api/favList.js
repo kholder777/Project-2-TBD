28f177c2dd86c34cd519eedeb089e25f4db0f8d5
Kevin Holder <kholder777@yahoo.com>
2020-08-08 11:50:00 -0500

    Added fav api to items.js

--- controllers/api/favList.js
+++ controllers/api/favList.js
@@ -1,8 +1,24 @@
-const router = require('express').Router();
-var db = require('../../models');
+const router = require("express").Router();
+var db = require("../../models");
 // get route -> index
-router.get('/', function (req, res) {
-  //do things here for other routes
+
+router.get("/fav", function (req, res) {
+	// replace old function with sequelize function
+	db.items
+		.findAll({
+			where: {
+				fav: true,
+			},
+		})
+		// use promise method to pass the userlist...
+		.then(function (dbitems) {
+			// into the main index, updating the page
+			console.log(dbitems);
+			var dbitems = {
+				items: dbitems,
+			};
+			return res.json(dbitems);
+		});
 });
 
 module.exports = router;
