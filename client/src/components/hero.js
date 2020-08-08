282d3dfc8a6992bf67af22b18f4faf610ab8e6ed
Ashley Lerma <lerma.scout@gmail.com>
2020-08-08 16:28:03 -0500

    updated h1 to be app specific and deleted unused className

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -5,19 +5,7 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
-
-    <p className="lead">
-      Check out our repo @{" "}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/The-CallBack-Cats"
-      >
-        The-CallBack-Cats
-      </a>
-      .
-    </p>
+    <h1>Welcome to BagIt </h1>
   </div>
 );
 
