bbcb306b28adcd3850aaf4cf874b129bd6f8e14b
Ashley Lerma <lerma.scout@gmail.com>
2020-08-08 16:05:57 -0500

    deleted old App.js file

--- client/src/app.js
+++ client/src/app.js
@@ -9,26 +9,26 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "./app.css";
 
 const App = () => {
-	const { isLoading } = useAuth0();
-	if (isLoading) {
-		return <Loading />;
-	}
+  const { isLoading } = useAuth0();
+  if (isLoading) {
+    return <Loading />;
+  }
 
-	return (
-		<div id="app" className="d-flex flex-column h-100">
-			<NavBar />
-			<Container className="flex-grow-1 mt-5">
-				<Switch>
-					{/* <PrivateRuate path="/" exact component={Currentlist} /> */}
-					<Route path="/" exact component={Home} />
-					<PrivateRuate path="/profile" component={Profile} />
-					<Route path="/external-api" component={ExternalApi} />
-					<PrivateRuate path="/currentlist" component={Currentlist} />
-				</Switch>
-			</Container>
-			<Footer />
-		</div>
-	);
+  return (
+    <div id="app" className="d-flex flex-column h-100">
+      <NavBar />
+      <Container className="flex-grow-1 mt-5">
+        <Switch>
+          {/* <PrivateRuate path="/" exact component={Currentlist} /> */}
+          <Route path="/" exact component={Home} />
+          <PrivateRuate path="/profile" component={Profile} />
+          <Route path="/external-api" component={ExternalApi} />
+          <PrivateRuate path="/currentlist" component={Currentlist} />
+        </Switch>
+      </Container>
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
