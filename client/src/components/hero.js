9f1ef058505b6eecf3cbc4ad679858cc198ce264
Kevin Holder <kholder777@yahoo.com>
2020-08-08 16:45:20 -0500

    changed app logo.

--- client/src/components/hero.js
+++ client/src/components/hero.js
@@ -1,12 +1,11 @@
 import React from "react";
 
-import logo from "../assets/logo.svg";
-
+import logo from "../assets/shoppingPerson.png";
 const Hero = () => (
-  <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1>Welcome to BagIt </h1>
-  </div>
+	<div className="text-center hero my-5">
+		<img className="mb-3 app-logo" src={logo} alt="BagIt logo" width="120" />
+		<h1>Welcome to BagIt </h1>
+	</div>
 );
 
 export default Hero;
