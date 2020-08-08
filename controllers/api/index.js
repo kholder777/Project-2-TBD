2ed14e7dc472062f35a2af6f817d74c7fbdbb6fd
juanlajara <juanlajara001@gmail.com>
2020-08-08 15:43:14 -0500

    fixed broken fave ref in API routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,8 @@
 
 const router = require("express").Router();
 const currentListRoutes = require("./currentList");
-const favListRoutes = require("./favList");
 const userRoutes = require("./items");
 // Book routes
 router.use("/currentList", currentListRoutes);
-router.use("/favList", favListRoutes);
 router.use("/items", userRoutes);
 module.exports = router;
