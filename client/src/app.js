462179d78586e8de6be307f074150f77b11a6bf8
Ashley Lerma <lerma.scout@gmail.com>
2020-08-08 16:11:00 -0500

    deleted old commented out code for clarity

--- client/src/app.js
+++ client/src/app.js
@@ -32,17 +32,3 @@ const App = () => {
 };
 
 export default App;
-
-// // src/App.js
-// import React from "react";
-// import HomePage from "./views/home";
-
-// function App() {
-// 	return (
-// 		<div>
-// 			<HomePage />
-// 		</div>
-// 	);
-// }
-
-// export default App;
