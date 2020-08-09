c8742ab4c799e4aa2b43db8544afad301cbadd39
Jordan <jordan.a.neill@gmail.com>
2020-08-08 23:43:57 -0500

    taking out stickyfooter. it was causing issues bro

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,44 +1,21 @@
 import React from "react";
-import StickyFooter from "react-sticky-footer";
 
-const Footer = () => (
-  // <footer className="bg-dark p-3 text-center ">
-  //   <div className="logo" />
-  //   <p className="lead">
-  //     Check out our repo @{" "}
-  //     <a
-  //       target="_blank"
-  //       rel="noopener noreferrer"
-  //       href="https://github.com/The-CallBack-Cats"
-  //     >
-  //       The-CallBack-Cats
-  //     </a>
-  //     .
-  //   </p>
-  // </footer>
-  <StickyFooter
-    bottomThreshold={0}
-    normalStyles={{
-      backgroundColor: "#999999",
-      padding: "2rem",
-    }}
-    stickyStyles={{
-      backgroundColor: "rgba(000)",
-      padding: "2rem",
-    }}
-  >
-    <p className="lead">
-      Check out our repo @{" "}
-      <a
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://github.com/The-CallBack-Cats"
-      >
-        The-CallBack-Cats
-      </a>
-      .
-    </p>
-  </StickyFooter>
-);
+// const Footer = () => (
+// <footer className="bg-dark p-3 text-center ">
+//   <div className="logo" />
+//   <p className="lead">
+//     Check out our repo @{" "}
+//     <a
+//       target="_blank"
+//       rel="noopener noreferrer"
+//       href="https://github.com/The-CallBack-Cats"
+//     >
+//       The-CallBack-Cats
+//     </a>
+//     .
+//   </p>
+// </footer>
 
-export default Footer;
+// );
+
+// export default Footer;
