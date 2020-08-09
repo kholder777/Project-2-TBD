cf227a4cefc057f156b267952fcce608045aa6f6
Ashley Lerma <lerma.scout@gmail.com>
2020-08-09 13:08:38 -0500

    department styled as header

--- client/src/components/department.js
+++ client/src/components/department.js
@@ -3,17 +3,17 @@ import "./style.css";
 import ItemBar from "./itemBar";
 import { Container } from "react-bootstrap";
 function Department() {
-	const array = [1, 2, 3];
-	return (
-		<Container id="departmentContainer">
-			Department Name here!
-			{/* needs to know based on what the user picks in the drop down */}
-			{/* {DepartmentName} here... with hook? */}
-			{array.map((item) => {
-				return <ItemBar />;
-			})}
-		</Container>
-	);
+  const array = [1, 2, 3];
+  return (
+    <Container id="departmentContainer">
+      <h4>Department Name here!</h4>
+      {/* needs to know based on what the user picks in the drop down */}
+      {/* {DepartmentName} here... with hook? */}
+      {array.map((item) => {
+        return <ItemBar />;
+      })}
+    </Container>
+  );
 }
 
 export default Department;
