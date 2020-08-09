eca80a1f929b9ce2f850a342debd5f5a61881f32
Kevin Holder <kholder777@yahoo.com>
2020-08-09 14:50:54 -0500

    items array is showing up in console log so that we can make it a var.

--- client/src/components/department.js
+++ client/src/components/department.js
@@ -5,14 +5,15 @@ import ItemBar from "./itemBar";
 import { Container } from "react-bootstrap";
 
 function Department() {
+	var itemsArray = [];
 	const array = [1, 2, 3];
 	const [result] = useState(0);
 	useEffect(() => {
 		API.getAllItems()
-			.then((res) => console.log(res))
+			.then((res) => itemsArray.push(res.data.items))
 			.catch((err) => console.log(err));
 	}, [result]);
-
+	console.log(itemsArray);
 	return (
 		<Container id="departmentContainer">
 			<h4>Department Name here!</h4>
