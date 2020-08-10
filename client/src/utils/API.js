1db695215768f954fc574d5d603f8696db09aec1
juanlajara <juanlajara001@gmail.com>
2020-08-10 02:09:25 -0500

    + dynamic axios API call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+require("dotenv").config();
+const environment = process.env.NODE_ENV || "development";
 
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
 	getAllItems: function () {
+		if (environment === "development") {
+			return axios.get("http://localhost:3000/api/items/all");
+		}
 		return axios.get("https://bagit-and-tagit.herokuapp.com/api/items/all");
 	},
 	getAllCategories: function () {
