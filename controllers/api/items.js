f7a84c01094fc809791d210b9061bd4c269f185a
Jordan <jordan.a.neill@gmail.com>
2020-08-10 14:37:40 -0500

    I added a post to actually post/create a new item in the items table. It also adds categoryId and UserId with the new item name

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -3,75 +3,76 @@ var db = require("../../models");
 
 // get route, edited to match sequelize
 router.get("/active", function (req, res) {
-	// replace old function with sequelize function
-	db.items
-		.findAll({
-			where: {
-				active: 1,
-			},
-		})
-		// use promise method to pass the userlist...
-		.then(function (dbitems) {
-			// into the main index, updating the page
+  // replace old function with sequelize function
+  db.items
+    .findAll({
+      where: {
+        active: 1,
+      },
+    })
+    // use promise method to pass the userlist...
+    .then(function (dbitems) {
+      // into the main index, updating the page
 
-			var dbitems = {
-				items: dbitems,
-			};
-			return res.json(dbitems);
-		});
+      var dbitems = {
+        items: dbitems,
+      };
+      return res.json(dbitems);
+    });
 });
 
 router.get("/inactive", function (req, res) {
-	// replace old function with sequelize function
-	db.items
-		.findAll({
-			where: {
-				active: 0,
-			},
-		})
-		// use promise method to pass the userlist...
-		.then(function (dbitems) {
-			// into the main index, updating the page
+  // replace old function with sequelize function
+  db.items
+    .findAll({
+      where: {
+        active: 0,
+      },
+    })
+    // use promise method to pass the userlist...
+    .then(function (dbitems) {
+      // into the main index, updating the page
 
-			var dbitems = {
-				items: dbitems,
-			};
-			return res.json(dbitems);
-		});
+      var dbitems = {
+        items: dbitems,
+      };
+      return res.json(dbitems);
+    });
 });
 
 router.get("/all", function (req, res) {
-	// replace old function with sequelize function
-	db.items
-		.findAll({})
-		// use promise method to pass the userlist...
-		.then(function (dbitems) {
-			// into the main index, updating the page
+  // replace old function with sequelize function
+  db.items
+    .findAll({})
+    // use promise method to pass the userlist...
+    .then(function (dbitems) {
+      // into the main index, updating the page
 
-			var dbitems = {
-				items: dbitems,
-			};
-			return res.json(dbitems);
-		});
+      var dbitems = {
+        items: dbitems,
+      };
+      return res.json(dbitems);
+    });
 });
 
-router.get("/fav", function (req, res) {
-	// replace old function with sequelize function
-	db.items
-		.findAll({
-			where: {
-				fav: 1,
-			},
-		})
-		// use promise method to pass the userlist...
-		.then(function (dbitems) {
-			// into the main index, updating the page
+router.post("/", function (req, res) {
+  db.User.create({
+    user_name: req.body.user,
+  }).then((results) => {
+    db.items
+      .create({
+        items_name: req.body.item,
+        CategoryId: req.body.categoryId,
+        UserId: results.id,
+      })
 
-			var dbitems = {
-				items: dbitems,
-			};
-			return res.json(dbitems);
-		});
+      .then(function (dbitems) {
+        var dbitems = {
+          items: dbitems,
+        };
+        return res.json(dbitems);
+      });
+  });
 });
 
 module.exports = router;
